38f068d65786cf8c4950ae01bebfd9186c976b2e
pujesh <neelamsettipujeshgit@gmail.com>
2024-11-29 16:06:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,210 @@
+function redBtn (){
+    let t = document.getElementById("OwnText");
+    t.style.color = "red"
+}
+function greenBtn (){
+    let t = document.getElementById("OwnText");
+    t.style.color = "green"
+}
+function blueBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "blue"
+}
+function brownBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "brown"
+}
+function violetBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "violet"
+}
+function yellowBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "yellow"
+}
+function pinkBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "pink"
+}
+function orangeBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "orange"
+}
+function darkgoldenBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "darkgolden"
+}
+function darkslateblueBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "darkslateblue"
+}
+function darkcyanBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "darkcyan"
+}
+function darkgreenBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "darkgreen"
+}
+function maroonBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "maroon"
+}
+function crimsonBtn (){
+    let t = document.getElementById("OwnText");
+    t.style.color = "crimson"
+}
+function yellowgreenBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "yellowgreen"
+}
+function chocolateBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "chocolate"
+}
+function tealBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "teal"
+}
+function darkmagentaBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "darkmagenta"
+}
+function chartreuseBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "chartreuse"
+}
+function goldBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "gold"
+}
+function fuchsiaBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "fuchsia"
+}
+function aquaBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "aqua"
+}
+function olivedrabBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.color = "olivedrab"
+}
+function lightblueBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightblue"
+}
+function lightcoralBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightcoral"
+}
+function lightslategrayBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightslategray"
+}
+function lightseagreenBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightseagreen"
+}
+function lightslategreyBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightslategrey"
+}
+function lightsalmonBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightsalmon"
+}
+function lightgreenBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "lightgreen"
+}
+function mediumpurpleBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "mediumpurple"
+}
+function mediumturquoiseBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "mediumturquoise"
+}
+function wheatBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "wheat"
+}
+function thistleBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "thistle"
+}
+function peruBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "peru"
+}
+function palevioletredBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "palevioletred"
+}
+function rosybrownBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "rosybrown"
+}
+function dodgerblueBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "dodgerblue"
+}
+function slateblueBtn () {
+    let t = document.getElementById("OwnText");
+    t.style.background = "slateblue"
+}
+
+function algerian() {
+    let t = document.getElementById("OwnText");
+    let selectedValue = document.getElementById("fstyle").value;
+
+    if (selectedValue === "algerian") {
+        t.style.fontFamily = "Algerian"; 
+    }else if(selectedValue === "Agency_FB"){
+        t.style.fontFamily = "Agency FB";
+    }else if(selectedValue === "Kunstler_Script"){
+        t.style.fontFamily = "Kunstler Script";
+    }else if(selectedValue === "Old_English_Text_MT"){
+        t.style.fontFamily = "Old English Text MT";
+    }else if(selectedValue === "Times New Roman"){
+        t.style.fontFamily = "Times New Roman";
+    }else if(selectedValue === "Ink_Free"){
+        t.style.fontFamily = "Ink Free";
+    }else if(selectedValue === "Wide_Latin"){
+        t.style.fontFamily = "Wide Latin";
+    }else if(selectedValue === "Goudy_Stout"){
+        t.style.fontFamily = "Goudy Stout";
+    }else if(selectedValue === "Viner_Hand_ITC"){
+        t.style.fontFamily = "Viner Hand ITC";
+    }else if(selectedValue === "Vladimir_Script"){
+        t.style.fontFamily = "Vladimir Script";
+    }else if(selectedValue === "Snap_ITC"){
+        t.style.fontFamily = "Snap ITC";
+    }else if(selectedValue === "Rage_Italic"){
+        t.style.fontFamily = "Rage Italic";
+    }else if(selectedValue === "Goudy_Stout"){
+        t.style.fontFamily = "Goudy Stout";
+    }else if(selectedValue === "Eras_Bold ITC"){
+        t.style.fontFamily = "Eras Bold ITC";
+    }else if(selectedValue === "Joker_man"){
+        t.style.fontFamily = "Jokerman";
+    }else if(selectedValue === "Script_MT Bold"){
+        t.style.fontFamily = "Script MT Bold";
+    }else if(selectedValue === "Matura_MT Script Capitals"){
+        t.style.fontFamily = "Matura MT Script Capitals";
+    }else if(selectedValue === "Franklin_Gothic Demi Cond"){
+        t.style.fontFamily = "Franklin Gothic Demi Cond"
+    }else if(selectedValue === "Imprint_MT Shadow"){
+        t.style.fontFamily = "Imprint MT Shadow"
+    }
+
+    else {
+        t.style.fontFamily = ""; 
+    }
+}
+
+
+
+
+
